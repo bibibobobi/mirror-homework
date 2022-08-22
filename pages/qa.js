4807bc81192e7c4145801df2b92086296f691824
bibibobobi <smilingcat33@gmail.com>
2022-08-22 15:40:15 +0800

    finished

--- pages/qa.js
+++ pages/qa.js
@@ -1,4 +1,9 @@
+import { useState } from 'react';
 import { request } from 'graphql-request';
+
+import { FiPlus, FiMinus } from 'react-icons/fi';
+
+import styles from '../styles/qa.module.css';
 
 //request query
 const QAS = ` {  
@@ -32,20 +37,43 @@ const Qa = ({ data }) => {
   const qas = data[0];
   // console.log(qas.items);
 
+  const [clicked, setClicked] = useState(false);
+  const toggle = (index) => {
+    if (clicked === index) {
+      //If clicked question is already active, then close it
+      return setClicked(null);
+    }
+
+    setClicked(index);
+  };
+
   return (
-    <>
-      <h1>{qas.name}</h1>
-      <div>
-        {qas.items.map((item) => {
+    <div className={styles.accordionSection}>
+      <div className={styles.container}>
+        <div className={styles.title}>{qas.name}</div>
+        {qas.items.map((item, index) => {
           return (
-            <div key={item.id}>
-              <h2>question:{item.question}</h2>
-              <h2>answer:{item.answer}</h2>
-            </div>
+            <>
+              <div
+                key={index}
+                className={styles.questionWrap}
+                onClick={() => toggle(index)}
+              >
+                <h1 className={styles.question}>{item.question} </h1>
+                <span className={styles.icon}>
+                  {clicked === index ? <FiMinus /> : <FiPlus />}
+                </span>
+              </div>
+              {clicked === index ? (
+                <div className={styles.dropDown}>
+                  <p>{item.answer}</p>
+                </div>
+              ) : null}
+            </>
           );
         })}
       </div>
-    </>
+    </div>
   );
 };
 
